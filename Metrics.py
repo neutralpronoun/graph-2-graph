cf72526bfd39bcc296eef898de444b235d7e8de9
neutralpronoun <alexowendavies@gmail.com>
2023-07-30 22:06:51 +0100

    Possible GPU use fix - issues in sample_features, memory use increases over

--- Metrics.py
+++ Metrics.py
@@ -107,6 +107,7 @@ class ContinuousVectorMetrics:
             loss, x_pred = wrapper.sample_features(batch,
                                                    visualise = None,
                                                    gif_first = False)
+            print(x_pred)
             x_preds_batch.append(x_pred.detach().cpu())
 
             total_loss += loss.item() / batch.num_graphs
